8f9bdce530201c551de755971bbc0289b557a46e
tfkrytwmaj-lang <tfkrytwmaj@gmail.com>
2026-06-24 19:38:03 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 /* ======================
    MongoDB Connection
 ====================== */
